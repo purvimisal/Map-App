cea2451aa45584d53a6833cf2f2fd32c09f322a5
purvimisal <purvivmisal@gmail.com>
2019-11-14 15:09:30 -0800

    form added

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,9 @@
 import React, { Component } from 'react';
 import L from 'leaflet';
+import { Button, Form, FormGroup, Label, Input, FormText } from 'reactstrap';
+import {
+  Card, CardText, CardBody,
+  CardTitle, CardSubtitle} from 'reactstrap';
 import { Map, TileLayer, Marker, Popup } from 'react-leaflet';
 // import { Card, CardText, Button } from 'reactstrap';
 
@@ -22,7 +26,7 @@ class App extends Component {
       lat: 51.505,
       lng: -0.09,
     },
-    haveUsersLocation:false,
+    haveUsersLocation: false,
     zoom: 2
   }
 
@@ -33,24 +37,24 @@ class App extends Component {
           lat: position.coords.latitude,
           lng: position.coords.longitude
         },
-        haveUsersLocation:true,
+        haveUsersLocation: true,
         zoom: 13
       });
     }, () => {
       console.log("uh oh... user didnt give location access");
       fetch('https://ipapi.co/json')
-      .then(res => res.json())
-      .then(location => {
-        console.log(location);
-        this.setState({
-          location: {
-            lat: location.latitude,
-            lng: location.longitude
-          },
-          haveUsersLocation:true,
-          zoom: 13
+        .then(res => res.json())
+        .then(location => {
+          console.log(location);
+          this.setState({
+            location: {
+              lat: location.latitude,
+              lng: location.longitude
+            },
+            haveUsersLocation: true,
+            zoom: 13
+          });
         });
-      });
     });
   }
 
@@ -58,23 +62,38 @@ class App extends Component {
     const position = [this.state.location.lat, this.state.location.lng]
     return (
       <div className="map">
-      <Map className="map" center={position} zoom={this.state.zoom}>
-        <TileLayer
-          attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        />
-        {
-          this.state.haveUsersLocation ?
-        <Marker
-          position={position}
-          icon={myIcon}
-        >
-          <Popup>
-            A pretty CSS3 popup. <br /> Easily customizable.
+        <Map className="map" center={position} zoom={this.state.zoom}>
+          <TileLayer
+            attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          />
+          {
+            this.state.haveUsersLocation ?
+              <Marker
+                position={position}
+                icon={myIcon}
+              >
+                <Popup>
+                  A pretty CSS3 popup. <br /> Easily customizable.
           </Popup>
-        </Marker> : ''
-        }
-      </Map>
+              </Marker> : ''
+          }
+        </Map>
+        <Card body className="message-form">
+          <CardTitle>Welcome to Map-App</CardTitle>
+          <CardSubtitle>Leave a message:</CardSubtitle>
+          <Form>
+            <FormGroup>
+              <Label for="Name">Name</Label>
+              <Input type="textarea" name="text" id="exampleText" />              
+            </FormGroup>
+            <FormGroup>
+              <Label for="Message">Text Area</Label>
+              <Input type="textarea" name="text" id="exampleText" />              
+            </FormGroup>
+          </Form>
+          <Button>Submit</Button>
+        </Card>
       </div>
     )
   }
